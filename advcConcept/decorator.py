d9a2b3fb61bcb4c92cc9b521ac24e77f1a36b4b7
ShubhamGhosh1993 <shubham.ghoshofficial@gmail.com>
2023-12-18 19:53:29 +0530

    4th commit

--- advcConcept/decorator.py
+++ advcConcept/decorator.py
@@ -1,8 +1,16 @@
-def myDecorator():
-    def wrapper():
+def myDecorator(func):
+    def wrapper(*args, **kargs):
         print("I am decorating the function")
-        
+        return func(*args, **kargs)
     return wrapper
 
+@myDecorator
 def helloWorld():
     print("Hello World!")
+
+@myDecorator
+def greeting(person):
+    return f"Hello {person}"
+    
+helloWorld()
+print(greeting("Shubham"))
